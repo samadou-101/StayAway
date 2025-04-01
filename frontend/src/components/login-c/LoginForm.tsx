11cccdbd00d331b9cdcb3726f916b9dec799845f
samadou <mossabm036@gmail.com>
2025-04-01 09:24:47 +0100

    migrated login/signup

--- frontend/src/components/login-c/LoginForm.tsx
+++ frontend/src/components/login-c/LoginForm.tsx
@@ -1,6 +1,4 @@
-import style from "./styles/login.module.css";
-import { BiShow } from "react-icons/bi";
-import { BiHide } from "react-icons/bi";
+import { BiShow, BiHide } from "react-icons/bi";
 import Logo from "../../assets/no-background-logo.png";
 import { RootState } from "../../redux/store";
 import { useState } from "react";
@@ -21,7 +19,7 @@ const Login: React.FC = () => {
   } = useForm<Inputs>();
   const dispatch = useAppDispatch();
   const passwordVisible = useAppSelector(
-    (state) => state.login.passwordVisible
+    (state) => state.login.passwordVisible,
   );
   const [password, setPassword] = useState<string>("");
   const handleTogglePasswordVisibility = () => {
@@ -29,27 +27,31 @@ const Login: React.FC = () => {
   };
 
   return (
-    <main className={style["login-container"]}>
-      <div className={style["login-form"]}>
-        <span className={style["app-name"]}>
-          <img src={Logo} alt="" />
+    <main className="flex h-screen w-screen items-center justify-center bg-[#f1f5f9] pb-40">
+      <div className="flex w-fit flex-col items-center gap-4 rounded-lg bg-white p-4 px-12 pb-8 shadow-[0_4px_10px_rgba(0,0,0,0.2)]">
+        <span className="flex items-center gap-[0.4rem] pb-2 text-[2.5rem] font-bold">
+          <img src={Logo} alt="" className="h-14 w-14 rounded-full" />
           StayAway
         </span>
-        <div className={style["inputs"]}>
-          <label htmlFor="email">Email</label>
-          <div className={style["email-container"]}>
+        <div className="flex w-72 flex-col gap-[0.7rem]">
+          <label htmlFor="email" className="font-medium">
+            Email
+          </label>
+          <div className="flex w-full items-center justify-between rounded-[7px] border border-gray-300 bg-white">
             <input
-              className={style["email"]}
+              className="w-full rounded-[7px] border-none p-[0.3rem] outline-none"
               {...register("email", { required: "Email required!" })}
               id="email"
               type="text"
               placeholder="Email"
             />
           </div>
-          <label htmlFor="password"> Password</label>
-          <div className={style["password-container"]}>
+          <label htmlFor="password" className="font-medium">
+            Password
+          </label>
+          <div className="flex w-full items-center justify-between rounded-[7px] border border-gray-300 bg-white pr-[0.2rem]">
             <input
-              className={style["password"]}
+              className="w-[90%] border-none bg-transparent p-[0.3rem]"
               type={passwordVisible ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -59,32 +61,38 @@ const Login: React.FC = () => {
             {password !== "" &&
               (passwordVisible ? (
                 <BiShow
-                  className={style["show-hide"]}
+                  className="cursor-pointer"
                   onClick={handleTogglePasswordVisibility}
                   size={25}
-                  style={{ cursor: "pointer" }}
                 />
               ) : (
                 <BiHide
-                  className={style["show-hide"]}
+                  className="cursor-pointer"
                   onClick={handleTogglePasswordVisibility}
                   size={25}
-                  style={{ cursor: "pointer" }}
                 />
               ))}
           </div>
         </div>
-        <button className={style["login-btn"]}>Login</button>
-        <div className={style["or"]}>or</div>
-        <div className={style["log-with-google"]}>
+        <button className="w-full cursor-pointer rounded-[7px] border border-gray-300 p-[0.3rem] font-semibold">
+          Login
+        </button>
+        <div className="relative w-fit text-gray-500 before:absolute before:top-[60%] before:w-12 before:-translate-x-[120%] before:border before:border-gray-500 before:content-[''] after:absolute after:top-[60%] after:w-12 after:translate-x-[20%] after:border after:border-gray-500 after:content-['']">
+          or
+        </div>
+        <div className="flex w-full cursor-pointer justify-center gap-2 rounded-[7px] border border-gray-300 p-1">
           <img
             src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
             alt=""
+            className="h-6 w-6 rounded-full"
           />
           <span>Log in with Google</span>
         </div>
-        <p className={style["no-account"]}>
-          You don't have an account? <Link to={"/signup"}>signup </Link>
+        <p className="no-account">
+          You don't have an account?{" "}
+          <Link to={"/signup"} className="text-[#0077b6]">
+            signup
+          </Link>
         </p>
       </div>
     </main>
--- frontend/src/components/signup-c/SignupForm.tsx
+++ frontend/src/components/signup-c/SignupForm.tsx
@@ -1,4 +1,3 @@
-import style from "./styles/signup.module.css";
 import Logo from "../../assets/no-background-logo.png";
 import { BiHide, BiShow } from "react-icons/bi";
 import { Link } from "react-router-dom";
@@ -10,7 +9,7 @@ function SignupForm() {
   const [password, setPassword] = useState<string>("");
 
   const passwordVisible = useAppSelector(
-    (state) => state.signup.passwordVisible
+    (state) => state.signup.passwordVisible,
   );
   const dispatch = useAppDispatch();
   const handleTogglePasswordVisibility = () => {
@@ -18,35 +17,41 @@ function SignupForm() {
   };
 
   return (
-    <main className={style["signup-container"]}>
-      <div className={style["signup-form"]}>
-        <span className={style["app-name"]}>
-          <img src={Logo} alt="" />
+    <main className="flex h-screen w-screen items-center justify-center bg-[#f1f5f9] pb-40">
+      <div className="flex w-fit flex-col items-center gap-4 rounded-lg bg-white p-4 px-12 pb-8 shadow-[0_4px_10px_rgba(0,0,0,0.2)]">
+        <span className="flex items-center gap-[0.4rem] pb-2 text-[2.5rem] font-bold">
+          <img src={Logo} alt="" className="h-14 w-14 rounded-full" />
           StayAway
         </span>
-        <div className={style["inputs"]}>
-          <label htmlFor="username">Username</label>
-          <div className={style["username-container"]}>
+        <div className="flex w-72 flex-col gap-[0.7rem]">
+          <label htmlFor="username" className="font-medium">
+            Username
+          </label>
+          <div className="flex w-full items-center justify-between rounded-[7px] border border-gray-300 bg-white">
             <input
-              className={style["username"]}
+              className="w-full rounded-[7px] border-none p-[0.3rem] outline-none"
               id="username"
               type="text"
               placeholder="Username"
             />
           </div>
-          <label htmlFor="email">Email</label>
-          <div className={style["email-container"]}>
+          <label htmlFor="email" className="font-medium">
+            Email
+          </label>
+          <div className="flex w-full items-center justify-between rounded-[7px] border border-gray-300 bg-white">
             <input
-              className={style["email"]}
+              className="w-full rounded-[7px] border-none p-[0.3rem] outline-none"
               id="email"
               type="text"
               placeholder="Email"
             />
           </div>
-          <label htmlFor="password"> Password</label>
-          <div className={style["password-container"]}>
+          <label htmlFor="password" className="font-medium">
+            Password
+          </label>
+          <div className="flex w-full items-center justify-between rounded-[7px] border border-gray-300 bg-white pr-[0.2rem]">
             <input
-              className={style["password"]}
+              className="w-[90%] border-none bg-transparent p-[0.3rem]"
               type={passwordVisible ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -56,32 +61,38 @@ function SignupForm() {
             {password !== "" &&
               (passwordVisible ? (
                 <BiShow
-                  className={style["show-hide"]}
+                  className="cursor-pointer"
                   onClick={handleTogglePasswordVisibility}
                   size={25}
-                  style={{ cursor: "pointer" }}
                 />
               ) : (
                 <BiHide
-                  className={style["show-hide"]}
+                  className="cursor-pointer"
                   onClick={handleTogglePasswordVisibility}
                   size={25}
-                  style={{ cursor: "pointer" }}
                 />
               ))}
           </div>
         </div>
-        <button className={style["signup-btn"]}>Signup</button>
-        <div className={style["or"]}>or</div>
-        <div className={style["log-with-google"]}>
+        <button className="w-full cursor-pointer rounded-[7px] border border-gray-300 p-[0.3rem] font-semibold">
+          Signup
+        </button>
+        <div className="relative w-fit text-gray-500 before:absolute before:top-[60%] before:w-12 before:-translate-x-[120%] before:border before:border-gray-500 before:content-[''] after:absolute after:top-[60%] after:w-12 after:translate-x-[20%] after:border after:border-gray-500 after:content-['']">
+          or
+        </div>
+        <div className="flex w-full cursor-pointer justify-center gap-2 rounded-[7px] border border-gray-300 p-1">
           <img
             src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
             alt=""
+            className="h-6 w-6 rounded-full"
           />
           <span>Signup with Google</span>
         </div>
-        <p className={style["no-account"]}>
-          You don't have an account? <Link to={"/login"}>login</Link>
+        <p className="no-account">
+          Already have an account?{" "}
+          <Link to={"/login"} className="text-[#0077b6]">
+            login
+          </Link>
         </p>
       </div>
     </main>
