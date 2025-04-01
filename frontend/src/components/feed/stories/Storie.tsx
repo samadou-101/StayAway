6c64c2cc903ed42c11fac67bf9f6a51d91e58501
samadou <mossabm036@gmail.com>
2025-04-01 08:31:55 +0100

    style: migrated sotrie

--- frontend/src/components/feed/stories/Storie.tsx
+++ frontend/src/components/feed/stories/Storie.tsx
@@ -1,14 +1,17 @@
-import style from "../styles/stories.module.css";
-import pic from "../../../assets/nature.jpg";
 function Storie() {
   return (
-    <div className={style["storie-container-spin"]}>
-      <div className={style["story-container"]}>
+    <div
+      className="relative flex h-16 w-16 items-center justify-center rounded-full object-cover"
+      style={{
+        background:
+          "conic-gradient(#098952 0%, #fa7e1e 25%, #d62976 50%, #962fbf 75%, #4f5bd5 100%)",
+      }}
+    >
+      <div className="absolute flex h-[90%] w-[90%] cursor-pointer items-center justify-center rounded-full border-[3px] border-white bg-white">
         <img
-          src={
-            "https://imgcdn.stablediffusionweb.com/2024/9/8/9bc3b58a-aca9-4f88-9ecc-6ea2217f7790.jpg"
-          }
+          src="https://imgcdn.stablediffusionweb.com/2024/9/8/9bc3b58a-aca9-4f88-9ecc-6ea2217f7790.jpg"
           alt=""
+          className="h-full w-full rounded-full object-cover"
         />
       </div>
     </div>
--- frontend/src/components/feed/stories/StoriesSection.tsx
+++ frontend/src/components/feed/stories/StoriesSection.tsx
@@ -1,9 +1,9 @@
-import style from "../styles/stories.module.css";
 import Storie from "./Storie";
+
 function Stories() {
   return (
     <>
-      <div className={style["stories-container"]}>
+      <div className="mx-auto flex h-24 w-3/5 items-center justify-between rounded-xl bg-white px-10 shadow-[2px_3px_5px_1px_rgba(0,0,0,0.1)]">
         <Storie />
         <Storie />
         <Storie />
